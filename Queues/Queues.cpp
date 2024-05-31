caa1da0e10077bcdab0914a0bc682460be541d17
Pradipa Y <merakayamsstraat@gmail.com>
2024-05-31 10:43:23 +0700

    fungsi if else lanjut di void remove

--- Queues/Queues.cpp
+++ Queues/Queues.cpp
@@ -41,6 +41,16 @@ public:
 
 		cout << "\nThe Element Deleted from the queues is : " << queue_array[FRONT] << "\n";
 
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
 		
 		
 	}
